4b4ec07f87aa563c2722a39f1c4532c639bf8e6c
unknown <1515251@RG43035.rgu.ac.uk>
2016-02-03 16:32:25 +0000

    Initial commit

---
+++ AzureTest.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+/**
+ * Created by PhpStorm.
+ * User: 1515251
+ * Date: 03/02/2016
+ * Time: 15:31
+ */
+class AzureTest
+{<!DOCTYPE html>
+<html>
+<head lang="en">
+<meta charset="UTF-8">
+<title>Deployment Test</title>
+</head>
+<body>
+<H1> It's ALIVE!!!</H1>
+</body>
+</html>
+
+}
